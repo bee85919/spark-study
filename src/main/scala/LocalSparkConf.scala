9552585f1b52dbc5207f4d1001aa2fb6a885a2ed
bee85919 <bee85919@gmail.com>
2023-08-23 13:25:42 +0900

    Spark Study

---
+++ src/main/scala/LocalSparkConf.scala
@@ -0,0 +1,11 @@
+import org.apache.spark.SparkConf
+
+object LocalSparkConf {
+    val sparkHome = System.getenv("SPARK_HOME")
+    val logDir = s"file:${sparkHome}/event"
+    def get(appName: String): SparkConf = new SparkConf()
+      .setMaster("local[*]")
+      .setAppName(appName)
+      .set("spark.eventLog.enabled", "true")
+      .set("spark.eventLog.dir", logDir)
+}
